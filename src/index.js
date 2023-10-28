63a185636cc2ebbbea7b133279f6a8cafd59c686
Nikita K <rabot422@gmail.com>
2023-10-28 16:33:36 +0300

    create header with links

--- src/index.js
+++ src/index.js
@@ -3,12 +3,27 @@ import { homePage } from "./homePage/homePage"
 
 
 
-
-function renderPage(page){
-    const content = document.getElementById('content');
-    content.innerHTML = page()
-}
 function app(){
-    
+    const content = document.getElementById('content')
+    const header = document.createElement('header')
+    const main = document.createElement('main')
+    const footer = document.createElement('footer')
+    const homeLink = document.createElement('a')
+    const menuLink = document.createElement('a')
+    const contactLink = document.createElement('a')
+//          
+content.appendChild(header)
+    header.appendChild(homeLink)
+    header.appendChild(menuLink)
+    header.appendChild(contactLink)
+homeLink.textContent = 'HOME'
+menuLink.textContent = 'MENU'
+contactLink.textContent = 'CONTACT'
+    function renderPage(page){
+        main.innerHTML = page()
+    }
+console.log('WORK');
+
 }
-homePage()
+app()
+// homePage()
