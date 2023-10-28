2a9a2f7525b033d4d54d0592a49718c1e646e2e1
Nikita K <rabot422@gmail.com>
2023-10-28 13:18:11 +0300

    add gitignore and create renderPage

---
+++ main.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";console.log("hello world"),function(){const e=document.getElementById(e),t=(document.createElement("header"),document.createElement("main"));document.createElement("div"),e.appendChild(t),t.textContent="em"}()})();
