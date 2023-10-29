f3d89b2a5fb27bae7c47509781ad18dd7f164b44
Nikita K <rabot422@gmail.com>
2023-10-29 20:56:41 +0300

    add contact card and margin for fotter

--- src/contactPage/contactPage.js
+++ src/contactPage/contactPage.js
@@ -1,10 +1,28 @@
+import MANAGER_IMG from './../assets/images/japan-men.jpg'
+
 export function contactPage(){
 
     
     const contactTitle = document.createElement('h1');
     const contactWrap = document.createElement('div');
-    const container = document.createElement('div');
+    const contact = document.createElement('div');
     contactWrap.appendChild(contactTitle)
+    contactWrap.appendChild(contact)
+
+    contactWrap.classList.add('contactPage')
+    contact.classList.add('contact')
+    contactTitle.classList.add('contact_title')
+
+    contact.innerHTML = `
+            <div class="contact_row">
+                <h3 class="contact_name">Li-Joy</h3>
+                <div class="contact_position">Manager</div>
+                <div class="contact_number">+222 (22) 22-222-22</div>
+            </div>      
+            <img src=${MANAGER_IMG} alt="" class="contact_img">
+           
+    `
     contactTitle.textContent = 'CONTACT'
+
     return contactWrap
 }
--- src/menuPage.js/menuPage.js
+++ src/menuPage.js/menuPage.js
@@ -1,8 +1,6 @@
-
-import IMG_FOR_MENU from './../assets/images/sushi-for-menu.jpg'
-import IMG_FOR_MENU2 from './../assets/images/fish-roe.jpg'
-import IMG_FOR_MENU3 from './../assets/images/salmon.jpg'
-
+import IMG_FOR_MENU from './../assets/images/sushi-for-menu.jpg';
+import IMG_FOR_MENU2 from './../assets/images/fish-roe.jpg';
+import IMG_FOR_MENU3 from './../assets/images/salmon.jpg';
 
 export function menuPage() {
   const menuTitle = document.createElement('h1');
@@ -17,30 +15,28 @@ export function menuPage() {
     }
   }
   menuWrap.classList.add('menuPage');
-  menuTitle.classList.add('menu_title')
+  menuTitle.classList.add('menu_title');
   menuWrap.appendChild(menuTitle);
 
   menuTitle.textContent = 'MENU';
 
-    menu.push(new Dish('Sushi', 5, IMG_FOR_MENU ));
-    menu.push(new Dish('Fish roe', 8, IMG_FOR_MENU2 ))
-    menu.push(new Dish('Salmon sushi', 10, IMG_FOR_MENU3 ))
-  
-    menu.map((e) =>{
-        const item = document.createElement('div')
-        item.innerHTML = ` 
+  menu.push(new Dish('Sushi', 5, IMG_FOR_MENU));
+  menu.push(new Dish('Fish roe', 8, IMG_FOR_MENU2));
+  menu.push(new Dish('Salmon sushi', 10, IMG_FOR_MENU3));
+
+  menu.map((e) => {
+    const item = document.createElement('div');
+    item.innerHTML = ` 
             <div class="menu_row">
                 <h3 class="menu_item_title">${e.name}</h3>
                 <div class="menu_item_price">price:${e.price}</div>
             </div>      
             <img src=${e.img} alt="" class="menu_item_img">
      
-        `
-        item.classList.add('item_wrap')
-        menuWrap.appendChild(item)
-    })
-   
-
+        `;
+    item.classList.add('item_wrap');
+    menuWrap.appendChild(item);
+  });
 
   return menuWrap;
 }
