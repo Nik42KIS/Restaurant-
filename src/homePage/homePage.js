2719797df415cea31c4c7790afbf180e330ed4c1
Nikita K <rabot422@gmail.com>
2023-10-29 14:55:10 +0300

    finish with home page and style it

--- src/homePage/homePage.js
+++ src/homePage/homePage.js
@@ -1,13 +1,33 @@
+import LOGO from './../assets/images/logo-sushi.png';
+import SUSHI_FROM_RIGHT from './../assets/images/sushi1.png';
+
 export function homePage() {
+  const sushiLogo = new Image();
+  sushiLogo.src = LOGO;
+  sushiLogo.classList.add('logo');
+
+  const sushiMainImg = new Image();
+  sushiMainImg.src = SUSHI_FROM_RIGHT;
+  sushiMainImg.classList.add('sushi_main_img');
+
   const homeTitle = document.createElement('h1');
   const homeWrap = document.createElement('div');
   const container = document.createElement('div');
-  const description = document.createElement('div')
-  homeWrap.classList.add('homePage')
+  const description = document.createElement('div');
+  const location = document.createElement('div');
+  location.classList.add('location');
+  homeWrap.classList.add('homePage');
+  description.classList.add('description');
+  homeTitle.classList.add('home_title')
   homeWrap.appendChild(homeTitle);
   homeWrap.appendChild(description);
-  description.textContent = "SavorTokyo is a unique Japanese culinary destination where you can savor the flavors of Tokyo right in your city. Our menu features a variety of sushi, sashimi, teriyaki, and other dishes prepared using fresh ingredients and inspired by Tokyo street food. We offer an unparalleled experience that combines Japanese taste with contemporary style. At SavorTokyo, you'll discover the authentic taste of Tokyo in every bite."
-  description.classList.add('description')
-  homeTitle.textContent = 'SavorTokyo';
+  homeWrap.appendChild(location);
+  description.textContent =
+    'SavorTokyo: Authentic Tokyo flavors in your city, featuring sushi, sashimi, teriyaki, and more. Taste Tokyo today!';
+  description.after(sushiMainImg);
+  location.innerHTML = `<h2 class="location_title">Location</h2>
+  <div class="location_text">123 Forest Drive, Forestville, Maine</div>`;
+  homeTitle.innerHTML = `<div>Savor Tokyo</div>`;
+  homeTitle.appendChild(sushiLogo);
   return homeWrap;
 }
